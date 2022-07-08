7a31e0128fa0188cae888499405dcef6325c0e36
Vika <vpredko@mail.ru>
2022-07-08 21:13:40 +0300

    Solve task1

---
+++ task1/Program.cs
@@ -0,0 +1,14 @@
+﻿int Step (int a, int b)
+{
+    int result = 1;
+    for (int i=1; i<=b; i++)
+        result = result*a;
+        return result;
+}
+Console.WriteLine ("Введите число");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите степень");
+int B = Convert.ToInt32(Console.ReadLine());
+int res = Step(A,B);
+Console.WriteLine ($"{A}, {B} -> {res}");
+
