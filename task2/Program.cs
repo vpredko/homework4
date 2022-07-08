eda9dc750106d560532b077eaef013dda1f7a6f4
Vika <vpredko@mail.ru>
2022-07-08 21:21:57 +0300

    Solve task2

---
+++ task2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine ("Введите число");
+int number = Convert.ToInt32 (Console.ReadLine());
+Console.Write(number + "->");
+int sum = 0;
+while (number>0)
+{
+    sum = sum + (number % 10);
+    number = number/10;
+}
+Console.WriteLine(sum);
+
